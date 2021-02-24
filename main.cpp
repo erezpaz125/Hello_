cfb81a19fd04afa0b3cf4ba749cc1732ae5cd1d8
ron2244 <ron6@post.bgu.ac.il>
2021-02-24 13:56:48 +0200

    I added Z

--- main.cpp
+++ main.cpp
@@ -8,5 +8,7 @@ int main() {
     cout << "x value is - " << x << endl;
     int y=7;
     cout << "y value is - " << 7 << endl;
+    int z=x+y;
+    cout << "z value is - " << z << endl;
     return 0;
 }
