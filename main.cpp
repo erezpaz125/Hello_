7ea3c09e4f8daa074b1571cedc812d6f90cd16dc
erezpaz125 <erezpaz125@gmail.com>
2021-02-24 13:21:51 +0200

    hello

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << endl;
+    int x = 5;
+    x = x + 10;
+    cout << "x value is - " << x << endl;
+    return 0;
+}
